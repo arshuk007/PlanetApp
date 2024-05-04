76d9729c88bc7a855bdf1200ce99dd9921da41c0
Arshad <arshuk007@gmail.com>
2024-05-04 22:32:36 +0530

    on back click handled

--- app/src/main/java/com/test/planetapp/activity/MainActivity.kt
+++ app/src/main/java/com/test/planetapp/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.test.planetapp.activity
 
+import android.app.ActionBar
 import android.app.Activity
 import android.os.Bundle
+import android.window.OnBackInvokedDispatcher
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.test.planetapp.R
 import com.test.planetapp.databinding.ActivityMainBinding
@@ -13,6 +16,10 @@ import com.test.planetapp.fragment.HomeFragment
 class MainActivity : BaseActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+
+    val currentFragment: Fragment?
+        get() = supportFragmentManager.findFragmentById(R.id.container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +32,20 @@ class MainActivity : BaseActivity() {
 
     private fun init(){
         showHomeFragment()
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     private fun showHomeFragment(){
         addFragment(HomeFragment.newInstance(), HomeFragment::class.java.simpleName)
     }
 
-    fun Activity.onBackButtonPressed(callback: (() -> Boolean)) {
-        (this as? FragmentActivity)?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                if (!callback()) {
-                    remove()
-                    performBackPress()
-                }
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if(currentFragment is HomeFragment){
+                finish()
+            }else{
+                supportFragmentManager.popBackStack()
             }
-        })
-    }
-
-    fun Activity.performBackPress() {
-        (this as? FragmentActivity)?.onBackPressedDispatcher?.onBackPressed()
+        }
     }
 }
